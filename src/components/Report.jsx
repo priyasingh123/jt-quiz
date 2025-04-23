7b498ecc91f3985a244d1909ed3122d28fa208ff
priya <priyaasinghh345@gmail.com>
2025-04-23 12:29:58 +0530

    unescape characters and css

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react'
 import "./../index.css"
+import { decode } from 'he';
 
 const Report = ({questionBank, totalQues, username}) => {
   const [correctNo, setCorrectNo] = useState(0);
@@ -54,9 +55,9 @@ const Report = ({questionBank, totalQues, username}) => {
                     
                     <div key={index} className={`review ${reviewClass}`}>
                         <p className={`${reviewClass}-p`}>{String(reviewClass).toUpperCase()}</p>
-                        <p className={`review-ques`}>{`${index+1}. ${ques.question}`}</p>
+                        <p className={`review-ques`}>{`${index+1}. ${decode(ques.question)}`}</p>
                         {totalQues[index]?.mcq?.map ((option) => 
-                           <div className={`review-ques`}><input type="radio" disabled value={option} />{option}</div>
+                           <div className={`review-ques`}><input type="radio" disabled value={option} />{decode(option)}</div>
                         )}
                         <p className={`${reviewClass}-p`}>Your Answer: {totalQues[index].attempted}</p>
                         {reviewClass !== 'correct' && <p className={`correct-p`}>Correct Answer: {questionBank[index].correct_answer}</p>}
